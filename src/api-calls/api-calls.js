07c5859955dedb247393287bb07c69ff266921ed
Jack Laird <jack.laird0@gmail.com>
2018-05-31 10:34:12 -0600

    Add http to local host url

--- src/api-calls/api-calls.js
+++ src/api-calls/api-calls.js
@@ -1,5 +1,5 @@
 export const fetchPokemonTypes = () => {
-  return fetch('localhost:3001/types')
+  return fetch('http://localhost:3001/types/')
     .then( response => response.json())
     .then( pokeTypes => pokeTypes)
     .catch( error => console.log(error))
