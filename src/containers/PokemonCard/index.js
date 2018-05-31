e6eff8eee590138aea4987f10884016e55ed64bc
Jack Laird <jack.laird0@gmail.com>
2018-05-31 13:31:58 -0600

    Display pokemon in click

--- src/containers/PokemonCard/index.js
+++ src/containers/PokemonCard/index.js
@@ -1,22 +1,48 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPokemon } from './../../api-calls/api-calls';
+import { displayPokemon, findPokemonType } from './../../api-calls/api-calls';
+import { addPokemon, selectedType } from './../../actions/index'
 
 class PokemonCard extends Component{
 
-  displayPokemon = async (pokemon) => {
-    const selectedPokemon = pokemon.map( async (pokeId) => {
-      const pokeData = await fetchPokemon(pokeId);
-      console.log(pokeData)
-      return pokeData
+  handleClick = async (pokemonType, type) => {
+    const fetchPokemon = await displayPokemon(pokemonType);
+    this.props.addPokemon(fetchPokemon);
+    console.log(this.props)
+    this.props.selectType(type);
+  }
+
+  displaySelectedPokemon = (type) => {
+    const pokeInfo = this.props.pokemon.map((poke) => {
+      return (
+        <div>
+          <p>Name: {poke.name}</p>
+          <p>Type: {type}</p>
+          <p>Weight: {poke.weight}</p>
+          <img src={poke.sprites.front_shiny} alt={poke.name}/>
+        </div>
+      )
     })
+    return pokeInfo
   }
 
   displayPokemonTypes = () => {
     const displayType = this.props.types.map(type => {
-      return (
-        <div onClick={() => this.displayPokemon(type.pokemon)}>{type.name}</div>
-      )
+      if (this.props.selectedType === type.name) {
+        return (
+          <div onClick={() => this.handleClick(type.pokemon, '')}>
+            {type.name}
+            {this.displaySelectedPokemon(type.name)}
+          </div>
+        )
+      } else {
+        return (
+          <div onClick={() => this.handleClick(type.pokemon, type.name)}>
+            {type.name}
+          </div>
+        )
+      }
     })
     return displayType
   }
@@ -31,7 +57,14 @@ class PokemonCard extends Component{
 }
 
 const mapStateToProps = (state) => ({
-  types: state.types
+  types: state.types,
+  pokemon: state.pokemon,
+  selectedType: state.selectedType
 })
 
-export default connect(mapStateToProps)(PokemonCard)
+const mapDispatchToProps = (dispatch) => ({
+  addPokemon: (pokemon) => dispatch(addPokemon(pokemon)),
+  selectType: (type) => dispatch(selectedType(type))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(PokemonCard)
