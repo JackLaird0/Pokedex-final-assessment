f652c44797e17657b82d3d1e95780678c9bd7df9
Jack Laird <jack.laird0@gmail.com>
2018-05-31 13:31:26 -0600

    Add pokemon and selected type to root reducer and remove fake

--- src/reducers/root-reducer.js
+++ src/reducers/root-reducer.js
@@ -1,10 +1,13 @@
 import { combineReducers } from 'redux'
 import fakeReducer from './fake-reducer'
 import { pokemonTypeReducer } from './pokemon-type-reducer'
+import { addPokemonReducer } from './add-pokemon-reducer'
+import { selectTypeReducer } from './select-type-reducer'
 
 const rootReducer = combineReducers({
-  fake: fakeReducer,
-  types: pokemonTypeReducer
+  types: pokemonTypeReducer,
+  pokemon: addPokemonReducer,
+  selectedType: selectTypeReducer
 })
 
 export default rootReducer
