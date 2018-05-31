c3a0877a3289b9b6991654caedaa0a368e0e2287
Jack Laird <jack.laird0@gmail.com>
2018-05-31 11:34:22 -0600

    Fetch pokemon data on click of a type

--- src/containers/PokemonCard/index.js
+++ src/containers/PokemonCard/index.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { fetchPokemon } from './../../api-calls/api-calls';
 
 class PokemonCard extends Component{
+
+  displayPokemon = async (pokemon) => {
+    const selectedPokemon = pokemon.map( async (pokeId) => {
+      const pokeData = await fetchPokemon(pokeId);
+      console.log(pokeData)
+      return pokeData
+    })
+  }
 
   displayPokemonTypes = () => {
     const displayType = this.props.types.map(type => {
       return (
-        <button>{type.name}</button>
+        <div onClick={() => this.displayPokemon(type.pokemon)}>{type.name}</div>
       )
     })
     return displayType
